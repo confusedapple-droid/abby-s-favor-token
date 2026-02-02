173c2a8c3671f3b6d84fddec435a02b544255749
confusedapple-droid <jerknartea@gmail.com>
2026-02-02 17:57:07 +0800

    Change service name in worker response

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     return new Response(JSON.stringify({
       status: "ok",
-      service: "favor-cards-worker",
+      service: "index.html",
       time: new Date().toISOString()
     }), {
       headers: { "content-type": "application/json" }
